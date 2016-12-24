69cd63375ce35cfeeb45c948f8c2a9870dd24ad9
tphien <tranphuochien96@gmail.com>
2016-12-24 09:45:13 +0700

    setup UI: celebrate

---
+++ app/src/main/java/com/example/op/fitnessdiary/Celebrate/Celebrate_header.java
@@ -0,0 +1,40 @@
+package com.example.op.fitnessdiary.Celebrate;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.op.fitnessdiary.R;
+
+/**
+ * Created by OP on 12/24/2016.
+ */
+
+public class Celebrate_header extends Fragment {
+    private Celebrate_main mainCelebrate;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Đọc file xml tạo ra đối tượng View.
+
+        // inflate(@LayoutRes int resource, @Nullable ViewGroup root, boolean attachToRoot)
+
+        View view= inflater.inflate(R.layout.activity_celebration_header, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof Celebrate_main) {
+            this.mainCelebrate = (Celebrate_main) context;
+        }
+    }
+
+}
--- app/src/main/java/com/example/op/fitnessdiary/MainActivity.java
+++ app/src/main/java/com/example/op/fitnessdiary/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.op.fitnessdiary.Celebrate.Celebrate_main;
 import com.example.op.fitnessdiary.MainLogin.MainLogin;
 
 
@@ -29,6 +30,24 @@ public class MainActivity extends AppCompatActivity {
                 // Tạo một Intent:
                 // (Mang nội dung sẽ gửi tới Example1Activity).
                 Intent myIntent = new Intent(MainActivity.this, MainLogin.class);
+
+                // Yêu cầu chạy Example1Activity.
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        // Lấy ra button theo ID
+        Button button2 = (Button) this.findViewById(R.id.button_celebrate);
+
+        // Sét đặt sự kiện Click vào Button1.
+        button2.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // Tạo một Intent:
+                // (Mang nội dung sẽ gửi tới Example1Activity).
+                Intent myIntent = new Intent(MainActivity.this, Celebrate_main.class);
 
                 // Yêu cầu chạy Example1Activity.
                 MainActivity.this.startActivity(myIntent);
