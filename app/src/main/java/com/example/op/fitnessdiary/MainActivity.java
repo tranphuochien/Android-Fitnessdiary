fbc4f5d98af94362661552108bfe08d5214574ba
AndrejLee <ledinhhan00@gmail.com>
2016-12-27 10:47:28 +0700

    merge to UI

--- app/src/main/java/com/example/op/fitnessdiary/MainActivity.java
+++ app/src/main/java/com/example/op/fitnessdiary/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.op.fitnessdiary;
 
-<<<<<<< HEAD
 import android.widget.Button;
 
-=======
->>>>>>> 7c7cefe53334cdb083c26efcb5d1090889d5936a
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,16 +19,10 @@ import com.example.op.fitnessdiary.Plan.Plan_main;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mySetting;
-    private Button myBase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        this.mySetting = (Button) this.findViewById(R.id.test_intent_to_setting);
-        this.myBase = (Button) this.findViewById(R.id.test_intent_to_baseStats);
     }
 
     public void onClickSetting(View v){
@@ -49,65 +40,24 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(myIntent);
     }
 
-    public void onClickEvent(View v){
+    public void onClickEvents(View v){
         Intent myIntent = new Intent(this, CreateEvent_main.class);
         this.startActivity(myIntent);
-=======
 
+    }
 
-        // Lấy ra button theo ID
-        Button button1 = (Button) this.findViewById(R.id.button_signin);
+    public void onClickPlan(View v){
+        Intent myIntent = new Intent(this, Plan_main.class);
+        this.startActivity(myIntent);
+    }
 
-        // Sét đặt sự kiện Click vào Button1.
-        button1.setOnClickListener(new Button.OnClickListener() {
+    public void onClickLogin(View v){
+        Intent myIntent = new Intent(this, MainLogin.class);
+        this.startActivity(myIntent);
+    }
 
-            @Override
-            public void onClick(View v) {
-
-                // Tạo một Intent:
-                // (Mang nội dung sẽ gửi tới Example1Activity).
-                Intent myIntent = new Intent(MainActivity.this, MainLogin.class);
-
-                // Yêu cầu chạy Example1Activity.
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-
-        // Lấy ra button theo ID
-        Button button2 = (Button) this.findViewById(R.id.button_celebrate);
-
-        // Sét đặt sự kiện Click vào Button1.
-        button2.setOnClickListener(new Button.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                // Tạo một Intent:
-                // (Mang nội dung sẽ gửi tới Example1Activity).
-                Intent myIntent = new Intent(MainActivity.this, Celebrate_main.class);
-
-                // Yêu cầu chạy Example1Activity.
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-
-        // Lấy ra button theo ID
-        Button button3 = (Button) this.findViewById(R.id.button_plan);
-
-        // Sét đặt sự kiện Click vào Button1.
-        button3.setOnClickListener(new Button.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                // Tạo một Intent:
-                // (Mang nội dung sẽ gửi tới Example1Activity).
-                Intent myIntent = new Intent(MainActivity.this, Plan_main.class);
-
-                // Yêu cầu chạy Example1Activity.
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
->>>>>>> 7c7cefe53334cdb083c26efcb5d1090889d5936a
+    public void onClickCelebrate(View v){
+        Intent myIntent = new Intent(this, Celebrate_main.class);
+        this.startActivity(myIntent);
     }
 }
