2f693092008c0cac97b47423fd4f29d69865a1ac
tphien <tranphuochien96@gmail.com>
2016-12-30 16:50:23 +0700

    setup DB for account table

---
+++ app/src/main/java/com/example/op/fitnessdiary/MainLogin/MainLogin_LoginSuccess.java
@@ -0,0 +1,31 @@
+package com.example.op.fitnessdiary.MainLogin;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.op.fitnessdiary.R;
+import com.example.op.fitnessdiary.setting.Account_setting_main_fix;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainLogin_LoginSuccess extends AppCompatActivity implements  GoogleApiClient.OnConnectionFailedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_login_login_success);
+
+    }
+    public void onClickSignout(View v){
+        Intent myIntent = new Intent(this, MainLogin.class);
+        this.startActivity(myIntent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
